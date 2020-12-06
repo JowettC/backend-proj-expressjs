03ee0cef5b784e5afc50f0cd630bc5ddbe3f2e51
JowettC <57623021+JowettC@users.noreply.github.com>
2020-12-06 09:59:25 +0800

    cleaner code

--- index.js
+++ index.js
@@ -22,26 +22,23 @@ app.get('/',(req,res) =>{
     res.send('Hello World!!!');
 
 });
+// read
 app.get('/api/courses',(req,res) =>{
     // get data from database and send object instead
     res.send(courses);
 });
-
+// read
 app.get('/api/courses/:id',(req,res) =>{
     // get data from database and send object instead
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with the given id was not found') //404 if dont exist
+    if (!course) return res.status(404).send('The course with the given id was not found') //404 if dont exist
     res.send(course);
 });
-
+// create
 app.post('/api/courses',(req,res)=>{
     // validation
     const {error} = validateCourse(req.body);
-    if(error){
-        // 404 bad request
-        res.status(404).send(error.details[0].message)
-        return;
-    }
+    if(error) return res.status(404).send(error.details[0].message)
 
     const course = {
         id: courses.length+1,
@@ -50,20 +47,28 @@ app.post('/api/courses',(req,res)=>{
     courses.push(course);
     res.send(course);
 });
+// update
 app.put('/api/courses/:id',(req,res)=>{
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with the given id was not found') //404 if dont exist
+    if (!course) return res.status(404).send('The course with the given id was not found') //404 if dont exist
     // validing
 
     const {error} = validateCourse(req.body);
-    if(error){
-        // 404 bad request
-        res.status(404).send(error.details[0].message)
-        return;
-    }
+    if(error) return res.status(404).send(error.details[0].message)
+        
+    
     course.name = req.body.name;
     res.send(course);
 
+})
+
+app.delete('/api/courses/:id', (req,res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course with the given id was not found') //404 if dont exist
+    const index = courses.indexOf(course);
+    courses.splice(index,1)
+
+    res.send(course)
 })
 function validateCourse(course){
     const schema =Joi.object({
